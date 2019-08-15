8c70809a1409664f783cd4ac700512b8294dc5ca
nik frank <nikbfrank@gmail.com>
2019-08-15 05:25:01 -0700

    started record

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import emailRegex from './emailRegex';
+import goldRecord from './goldRecord.png';
 
 class App extends React.Component {
 
@@ -29,6 +30,8 @@ class App extends React.Component {
       isEmailValid: emailRegex.test( event.target.value ),
     })
 
+  setAlbumSales = event=> this.setState({ albumSales: event.target.value })
+
   render() {
     return (
       <div className="App">
@@ -50,6 +53,21 @@ class App extends React.Component {
                   <span className='invalid-email'>
                     Please enter a valid email
                   </span>
+                ) : null
+              }
+            </label>
+          </div>
+
+          <div className='card swanky-input-container'>
+            <label>
+              <input value={this.state.albumSales}
+                     type='number'
+                     step={10000}
+                     onChange={this.setAlbumSales} />
+              <span className='title'>Album Sales</span>
+              {
+                this.state.albumSales >= 1000000 ? (
+                  <img src={goldRecord} />
                 ) : null
               }
             </label>
