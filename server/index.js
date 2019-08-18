86fb0f1fb5a20a24500a5d50ee5eefd0b4d7e42b
nik frank <nikbfrank@gmail.com>
2019-08-18 06:34:18 -0700

    started server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+app.get('/', (req, res)=> {
+  res.json({ great: 'success' });
+});
+
+app.listen(port, ()=> console.log('listening on '+port));
